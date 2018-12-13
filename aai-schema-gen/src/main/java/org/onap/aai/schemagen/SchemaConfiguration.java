e8b34259cc66158738dda6fa425c8ebac729767e
Kajur, Harish (vk250x) <vk250x@att.com>
2018-12-13 08:37:13 -0500

    Move the aai-schema, annotations and
    schema generation code from the aai-common
    repo to the schema service repo as aai-schema
    should be in the schema service and all code
    related to it such as generation of the schema
    code should also be moved to the service
    
    Issue-ID: AAI-2002
    Change-Id: Ia792d5ae8822642b1816a0e9664bb21fa511e24f
    Signed-off-by: Kajur, Harish (vk250x) <vk250x@att.com>

---
+++ aai-schema-gen/src/main/java/org/onap/aai/schemagen/SchemaConfiguration.java
@@ -0,0 +1,50 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Modifications Copyright © 2018 IBM.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.aai.schemagen;
+
+import org.onap.aai.edges.EdgeIngestor;
+import org.onap.aai.nodes.NodeIngestor;
+import org.onap.aai.setup.AAIConfigTranslator;
+import org.onap.aai.setup.ConfigTranslator;
+import org.onap.aai.setup.SchemaLocationsBean;
+import org.onap.aai.setup.SchemaVersions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SchemaConfiguration {
+
+    @Bean
+    public EdgeIngestor edgeIngestor(SchemaLocationsBean schemaLocationsBean, SchemaVersions schemaVersions){
+        return new EdgeIngestor(configTranslator(schemaLocationsBean, schemaVersions), schemaVersions);
+    }
+
+    @Bean(name = "nodeIngestor")
+    public NodeIngestor nodeIngestor(ConfigTranslator configTranslator) {
+        return new NodeIngestor(configTranslator);
+    }
+
+    @Bean(name = "configTranslator")
+    public ConfigTranslator configTranslator(SchemaLocationsBean schemaLocationsBean, SchemaVersions schemaVersions) {
+        return new AAIConfigTranslator(schemaLocationsBean, schemaVersions);
+    }
+}
