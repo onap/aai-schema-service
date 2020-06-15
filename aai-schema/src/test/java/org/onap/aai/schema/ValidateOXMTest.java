ea1285768ffdf581a76cd95c7f1af4a74240f58c
LaMont, William(wl2432) <wl2432@att.com>
2020-06-15 11:54:29 -0400

    remove javafx reference
    Issue-ID: AAI-2930
    Change-Id: I67c05d4cd8b05a1a85156e8287be24831bf6be70
    Signed-off-by: LaMont, William(wl2432) <wl2432@att.com>

--- aai-schema/src/test/java/org/onap/aai/schema/ValidateOXMTest.java
+++ aai-schema/src/test/java/org/onap/aai/schema/ValidateOXMTest.java
@@ -19,8 +19,6 @@
  */
 package org.onap.aai.schema;
 
-
-import javafx.beans.binding.BooleanBinding;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
