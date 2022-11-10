6e14c2a05d1b375f1dbc893d74c92511d036f219
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2022-11-10 20:45:04 +0000

    Update aai-schema-ingest dependency in the schema-service
    Issue-ID: AAI-3592
    
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
    Change-Id: I7e0a6f4577d2254d8f58956f4ad3ea39bd74dc8d

--- aai-schema-gen/src/main/java/org/onap/aai/schemagen/genxsd/ConfigTranslatorForDocs.java
+++ aai-schema-gen/src/main/java/org/onap/aai/schemagen/genxsd/ConfigTranslatorForDocs.java
@@ -22,17 +22,21 @@ package org.onap.aai.schemagen.genxsd;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.onap.aai.setup.ConfigTranslator;
+import org.onap.aai.setup.SchemaConfigVersions;
 import org.onap.aai.setup.SchemaLocationsBean;
 import org.onap.aai.setup.SchemaVersion;
-import org.onap.aai.setup.SchemaVersions;
 
 public class ConfigTranslatorForDocs extends ConfigTranslator {
 
-    public ConfigTranslatorForDocs(SchemaLocationsBean bean, SchemaVersions schemaVersions) {
-        super(bean, schemaVersions);
+    public ConfigTranslatorForDocs(SchemaLocationsBean bean, SchemaConfigVersions schemaConfigVersions) {
+        super(bean, schemaConfigVersions);
     }
 
     @Override
