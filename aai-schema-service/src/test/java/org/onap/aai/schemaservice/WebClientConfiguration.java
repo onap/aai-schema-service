b874ac51451336b6b3c39935b75020f67e3e1388
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2024-12-08 20:41:19 +0100

    Add checksum endpoint to schema-service
    - provide endpoint that allows consumers to check if schema creation is necessary
    - this can be used for the schema creation job to only run when the schema changed
    
    Issue-ID: AAI-4084
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
    Change-Id: I831f8c134b548f460eda26570d45f99f28e048a4

---
+++ aai-schema-service/src/test/java/org/onap/aai/schemaservice/WebClientConfiguration.java
@@ -0,0 +1,32 @@
+package org.onap.aai.schemaservice;
+
+import java.time.Duration;
+import java.util.Collections;
+
+import org.onap.aai.schemaservice.nodeschema.SchemaVersions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestConfiguration
+public class WebClientConfiguration {
+
+  @Lazy
+  @Bean
+  WebTestClient webTestClient(@LocalServerPort int port) {
+    return WebTestClient.bindToServer()
+      .baseUrl("http://localhost:" + port + "/aai/schema-service")
+      .responseTimeout(Duration.ofSeconds(300))
+      .defaultHeaders(headers -> {
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.set("X-FromAppId", "test");
+        headers.set("X-TransactionId", "someTransaction");
+        headers.setBasicAuth("AAI", "AAI");
+      })
+      .build();
+  }
+}
