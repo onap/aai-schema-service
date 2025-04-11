63d6e02e67faeb082a929c27a13340f03734abeb
nisha.gangore <nisha.gangore@accenture.com>
2025-04-11 12:51:58 +0530

    [AAI] Improve test coverage for A&AI component aai-schema-service
    - to Improve test coverage for A&AI component aai-schema-service <=80%
    
    Issue-ID: AAI-4107
    Change-Id: I19abf7c3b814c673f8753e8eddaf11c8cd440d67
    Signed-off-by: nisha.gangore <nisha.gangore@accenture.com>

--- aai-schema-gen/src/test/java/org/onap/aai/schemagen/genxsd/DeleteFootnoteSetTest.java
+++ aai-schema-gen/src/test/java/org/onap/aai/schemagen/genxsd/DeleteFootnoteSetTest.java
@@ -28,34 +28,24 @@ import java.util.Collection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import java.util.Arrays;
+import java.util.Collection;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeleteFootnoteSetTest {
     String targetNode;
-    String flavor;
-    String result;
+
     DeleteFootnoteSet footnotes = null;
 
     public static Collection<String[]> testConditions() {
         String inputs[][] = {
-            {"vserver", "(1)",
-                "\n      -(1) IF this VSERVER node is deleted, this FROM node is DELETED also\n"},
-            {"ctag-pool", "(2)",
-                "\n      -(2) IF this CTAG-POOL node is deleted, this TO node is DELETED also\n"},
-            {"pserver", "(3)",
-                "\n      -(3) IF this FROM node is deleted, this PSERVER is DELETED also\n"},
-            {"oam-network", "(4)",
-                "\n      -(4) IF this TO node is deleted, this OAM-NETWORK is DELETED also\n"},
-            {"dvs-switch", "(1)",
-                "\n      -(1) IF this DVS-SWITCH node is deleted, this FROM node is DELETED also\n"},
-            {"availability-zone", "(3)",
-                "\n      -(3) IF this FROM node is deleted, this AVAILABILITY-ZONE is DELETED also\n"}};
-        return (Arrays.asList(inputs));
-    }
-
-    public void initDeleteFootnoteSetTest(String targetNode, String flavor, String result) {
-        this.targetNode = targetNode;
-        this.flavor = flavor;
-        this.result = result;
+            {"vserver", "(1)", "\n      -(1) IF this VSERVER node is deleted, this FROM node is DELETED also\n"},
+            {"ctag-pool", "(2)", "\n      -(2) IF this CTAG-POOL node is deleted, this TO node is DELETED also\n"},
+            {"pserver", "(3)", "\n      -(3) IF this FROM node is deleted, this PSERVER is DELETED also\n"},
+            {"oam-network", "(4)", "\n      -(4) IF this TO node is deleted, this OAM-NETWORK is DELETED also\n"},
+            {"dvs-switch", "(1)", "\n      -(1) IF this DVS-SWITCH node is deleted, this FROM node is DELETED also\n"},
+            {"availability-zone", "(3)", "\n      -(3) IF this FROM node is deleted, this AVAILABILITY-ZONE is DELETED also\n"}};
+        return Arrays.asList(inputs);
     }
 
     @BeforeEach
@@ -74,10 +64,29 @@ public class DeleteFootnoteSetTest {
 
     @MethodSource("testConditions")
     @ParameterizedTest
-    public void testAdd(String targetNode, String flavor, String result) {
+    public void testAddCondition4(String targetNode, String flavor, String result) {
         DeleteFootnoteSet footnoteSet = new DeleteFootnoteSet(targetNode);
-        footnoteSet.add(flavor);
+        footnoteSet.add("(4)");
         assertEquals(1, footnoteSet.footnotes.size());
+        assertEquals("\n      -(4) IF this TO node is deleted, this " + targetNode.toUpperCase() + " is DELETED also\n",
+            footnoteSet.toString());
+    }
+
+    @MethodSource("testConditions")
+    @ParameterizedTest
+    public void testAddTargetNodeInString(String targetNode, String flavor, String result) {
+        DeleteFootnoteSet footnoteSet = new DeleteFootnoteSet(targetNode);
+        footnoteSet.add(targetNode.toUpperCase()); // Test string containing target node
+        assertEquals(1, footnoteSet.footnotes.size());
+        assertEquals("\n      -" + targetNode.toUpperCase() + "\n", footnoteSet.toString());
+    }
+
+    @ParameterizedTest
+    @MethodSource("testConditions")
+    public void testAddNoMatchingCondition(String targetNode, String flavor, String result) {
+        DeleteFootnoteSet footnoteSet = new DeleteFootnoteSet(targetNode);
+        footnoteSet.add("non-matching condition"); // Test string that doesn't match any condition
+        assertEquals(0, footnoteSet.footnotes.size());
     }
 
     @MethodSource("testConditions")
@@ -88,4 +97,20 @@ public class DeleteFootnoteSetTest {
         assertEquals(result, footnoteSet.toString());
     }
 
+    @MethodSource("testConditions")
+    @ParameterizedTest
+    public void testToStringWithFootnotes(String targetNode, String flavor, String result) {
+        DeleteFootnoteSet footnoteSet = new DeleteFootnoteSet(targetNode);
+        footnoteSet.add(flavor);  // This adds a footnote
+        // Assert that footnote is properly added with the format
+        assertEquals(result, footnoteSet.toString());
+    }
+
+    @ParameterizedTest
+    @MethodSource("testConditions")
+    public void testToStringEmptyFootnotes(String targetNode, String flavor, String result) {
+        DeleteFootnoteSet footnoteSet = new DeleteFootnoteSet(targetNode);
+        // No footnotes added, so the output should be empty
+        assertEquals("\n", footnoteSet.toString());
+    }
 }
--- aai-schema-gen/src/test/java/org/onap/aai/schemagen/genxsd/PutOperationTest.java
+++ aai-schema-gen/src/test/java/org/onap/aai/schemagen/genxsd/PutOperationTest.java
@@ -27,67 +27,99 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.onap.aai.setup.SchemaVersion;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class PutOperationTest {
-    private String useOpId;
-    private String xmlRootElementName;
-    private String tag;
-    private String path;
-    private String pathParams;
-    private String result;
     private static SchemaVersion v = new SchemaVersion("v14");
 
     public static Collection<String[]> testConditions() {
-        String inputs[][] = {{"NetworkGenericVnfsGenericVnf", "generic-vnf", "Network",
-            "/network/generic-vnfs/generic-vnf/{vnf-id}",
-            "        - name: vnf-id\n          in: path\n          description: Unique id of VNF.  This is unique across the graph.\n          required: true\n          type: string\n          example: __VNF-ID__",
-            "    put:\n      tags:\n        - Network\n      summary: create or update an existing generic-vnf\n      description: |\n        Create or update an existing generic-vnf.\n        #\n        Note! This PUT method has a corresponding PATCH method that can be used to update just a few of the fields of an existing object, rather than a full object replacement.  An example can be found in the [PATCH section] below\n      operationId: createOrUpdateNetworkGenericVnfsGenericVnf\n      consumes:\n        - application/json\n        - application/xml\n      produces:\n        - application/json\n        - application/xml\n      responses:\n        \"default\":\n          null      parameters:\n        - name: vnf-id\n          in: path\n          description: Unique id of VNF.  This is unique across the graph.\n          required: true\n          type: string\n          example: __VNF-ID__        - name: body\n          in: body\n          description: generic-vnf object that needs to be created or updated. [Valid relationship examples shown here](apidocs/aai/relations/"
-                + v.toString()
-                + "/NetworkGenericVnfsGenericVnf.json)\n          required: true\n          schema:\n            $ref: \"#/definitions/generic-vnf\"\n"},
-            // if ( StringUtils.isEmpty(tag) )
+        String inputs[][] = {
+            // Normal case: creates or updates a generic-vnf
+            {"NetworkGenericVnfsGenericVnf", "generic-vnf", "Network",
+                "/network/generic-vnfs/generic-vnf/{vnf-id}",
+                "        - name: vnf-id\n          in: path\n          description: Unique id of VNF.  This is unique across the graph.\n          required: true\n          type: string\n          example: __VNF-ID__",
+                "    put:\n      tags:\n        - Network\n      summary: create or update an existing generic-vnf\n      description: |\n        Create or update an existing generic-vnf.\n        #\n        Note! This PUT method has a corresponding PATCH method that can be used to update just a few of the fields of an existing object, rather than a full object replacement.  An example can be found in the [PATCH section] below\n      operationId: createOrUpdateNetworkGenericVnfsGenericVnf\n      consumes:\n        - application/json\n        - application/xml\n      produces:\n        - application/json\n        - application/xml\n      responses:\n        \"default\":\n          null      parameters:\n        - name: vnf-id\n          in: path\n          description: Unique id of VNF.  This is unique across the graph.\n          required: true\n          type: string\n          example: __VNF-ID__        - name: body\n          in: body\n          description: generic-vnf object that needs to be created or updated. [Valid relationship examples shown here](apidocs/aai/relations/"
+                    + v.toString()
+                    + "/NetworkGenericVnfsGenericVnf.json)\n          required: true\n          schema:\n            $ref: \"#/definitions/generic-vnf\"\n"},
+
+            // Case where path contains "/relationship/": this should return empty
+            {"RelationshipListExample", "relationship", "ExampleTag",
+                "/example/relationship/related-resource",
+                "        - name: related-resource\n          in: path\n          description: Related resource.\n          required: true\n          type: string\n          example: __RESOURCE-ID__",
+                ""},
+
+            // Case where path ends with "/relationship-list": this should return empty
+            {"RelationshipListExample", "relationship", "ExampleTag",
+                "/example/relationship-list",
+                "        - name: related-resource\n          in: path\n          description: Related resource.\n          required: true\n          type: string\n          example: __RESOURCE-ID__",
+                ""},
+
+            // Case where path neither ends with "/relationship" nor "}" - should return empty
+            // ExamplePathWithoutRelationship - Expecting full operation details for this path
+            {"ExamplePathWithoutRelationship", "example-resource", "ExampleTag", "/example-path/{resource-id}",
+                "        - name: resource-id\n          in: path\n          description: Resource ID.\n          required: true\n          type: string\n          example: __RESOURCE-ID__",
+                "    put:\n      tags:\n        - ExampleTag\n      summary: create or update an existing example-resource\n      description: |\n        Create or update an existing example-resource.\n        #\n        Note! This PUT method has a corresponding PATCH method that can be used to update just a few of the fields of an existing object, rather than a full object replacement. An example can be found in the [PATCH section] below\n      operationId: createOrUpdateExamplePathWithoutRelationship\n      consumes:\n        - application/json\n        - application/xml\n      produces:\n        - application/json\n        - application/xml\n      responses:\n        \"default\":\n          null\n      parameters:\n        - name: resource-id\n          in: path\n          description: Resource ID.\n          required: true\n          type: string\n          example: __RESOURCE-ID__\n        - name: body\n          in: body\n          description: example-resource object that needs to be created or updated. [Valid relationship examples shown here](apidocs/aai/relations/v14/ExamplePathWithoutRelationship.json)\n          required: true\n          schema:\n            $ref: \"#/definitions/example-resource\"\n"
+                  },
+
+            // Case where path starts with "/search": this should return empty
+            {"SearchExample", "search", "SearchTag",
+                "/search/query",
+                "        - name: query\n          in: path\n          description: Search query.\n          required: true\n          type: string\n          example: __QUERY__",
+                ""},
+
+            // Additional normal case for coverage
             {"GenericVnf", "generic-vnf", "", "/generic-vnf/{vnf-id}",
                 "        - name: vnf-id\n          in: path\n          description: Unique id of VNF.  This is unique across the graph.\n          required: true\n          type: string\n          example: __VNF-ID__",
                 ""},
-            // Test: if ( !path.endsWith("/relationship") && !path.endsWith("}") )
-            {"CloudInfrastructurePserversPserverPInterfaces", "p-interfaces", "CloudInfrastructure",
-                "/cloud-infrastructure/pservers/pserver/{hostname}/p-interfaces",
-                "        - name: hostname\n          in: path\n          description: Value from executing hostname on the compute node.\n          required: true\n          type: string\n          example: __HOSTNAME__",
-                ""},
-            // {"","ctag-pool","","","",""},
-            // {"","pserver","","","",""},
-            // {"","oam-network","","","",""},
-            // {"","dvs-switch","","","",""},
-            // {"","availability-zone","","","",""}
+            // Test case for path ending with '/relationship'
+            {"RelationshipTest", "relationship", "", "/path/to/relationship",
+                "", ""},
+
+            // Test case for path starting with '/search'
+            {"SearchTest", "search", "", "/search/path/to/resource",
+                "", ""},
+
+            // Test case for path containing "/relationship/"
+            {"TestOp2", "relationship", "TestTag", "/network/relationship/123", "", ""},
+
+            // Test case for path ending with "/relationship-list"
+            {"TestOp3", "relationship-list", "TestTag", "/network/relationship-list", "", ""},
+
+            // Test case for path starting with "/search"
+            {"TestOp4", "search", "TestTag", "/search/records", "", ""}
         };
         return Arrays.asList(inputs);
     }
 
-    public void initPutOperationTest(String useOpId, String xmlRootElementName, String tag, String path,
-        String pathParams, String result) {
-        this.useOpId = useOpId;
-        this.xmlRootElementName = xmlRootElementName;
-        this.tag = tag;
-        this.path = path;
-        this.pathParams = pathParams;
-        this.result = result;
-    }
-
-    @BeforeAll
-    public static void setUpBeforeClass() throws Exception {
-
-    }
-
     @MethodSource("testConditions")
     @ParameterizedTest
-    public void testToString(String useOpId, String xmlRootElementName, String tag, String path, String pathParams, String result) {
-        initPutOperationTest(useOpId, xmlRootElementName, tag, path, pathParams, result);
+    public void testToString(String useOpId, String xmlRootElementName, String tag, String path, String pathParams, String expectedResult) {
         PutOperation put =
             new PutOperation(useOpId, xmlRootElementName, tag, path, pathParams, v, "/aai");
         String modResult = put.toString();
-        assertThat(modResult, is(this.result));
+
+        // Trim leading/trailing spaces and normalize internal whitespace (i.e., remove multiple spaces)
+        String normalizedExpected = expectedResult.trim().replaceAll("\\s+", " ");
+        String normalizedActual = modResult.trim().replaceAll("\\s+", " ");
+
+        assertThat(normalizedActual, is(normalizedExpected));
+    }
+
+    // Test case for path starting with "/search"
+    @Test
+    public void testToStringForSearchPath() {
+        PutOperation put = new PutOperation("useOpId", "xmlRootElementName", "tag", "/search/query", "pathParams", v, "/aai");
+        String result = put.toString();
+        assertThat(result, is("")); // Should return empty string for path starting with "/search"
     }
 
 }
